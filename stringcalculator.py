d94ea5723170217a314fcf608093d46f3b0af0d5
unknown <davidperezjpa@gmail.com>
2021-02-24 16:22:24 -0600

    progress

--- stringcalculator.py
+++ stringcalculator.py
@@ -2,4 +2,13 @@
 
 def Add(a):
     # Add implementation
-    return 0
+    if a:
+
+        numArray = a.split(",")
+        result = 0
+        for num in numArray:
+            result += int(num)
+        print(result)
+        return result
+    else:
+        return 0
